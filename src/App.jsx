0a2f1fa9c69df05a88303733ee8fb8422b5ce832
Divyansh Kashyap <duvyansh728@gmail.com>
2025-03-25 06:48:10 +0530

    add tailwind and grid of 3x3

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="min-h-screen min-w-screen bg-black flex justify-center items-center">
+        <div className="border-2 border-zinc-400 rounded-md">
+          <div className="grid grid-cols-3 gap-4 p-4">
+            <div className="bg-zinc-100 rounded-sm  p-6 text-center">1</div>
+            <div className="bg-zinc-100 rounded-sm  p-6 text-center">2</div>
+            <div className="bg-zinc-100 rounded-sm  p-6 text-center">3</div>
+            <div className="bg-zinc-100 rounded-sm  p-6 text-center">4</div>
+            <div className="bg-zinc-100 rounded-sm  p-6 text-center">5</div>
+            <div className="bg-zinc-100 rounded-sm  p-6 text-center">6</div>
+            <div className="bg-zinc-100 rounded-sm  p-6 text-center">7</div>
+            <div className="bg-zinc-100 rounded-sm  p-6 text-center">8</div>
+            <div className="bg-red-600 rounded-sm  p-6 text-center">9</div>
+          </div>
+
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
