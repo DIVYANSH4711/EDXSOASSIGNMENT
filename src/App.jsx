27e927aec0bf691dcaf9ef82cd86effc5dce7fbe
Divyansh Kashyap <duvyansh728@gmail.com>
2025-03-25 07:45:22 +0530

    add classes and onClick func to button 1-8

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,76 @@
+import { useState } from "react"
+
 function App() {
+  const [order, setOrder] = useState([])
+  const [id1, setId1] = useState("zinc")
+  const [id2, setId2] = useState("zinc")
+  const [id3, setId3] = useState("zinc")
+  const [id4, setId4] = useState("zinc")
+  const [id5, setId5] = useState("zinc")
+  const [id6, setId6] = useState("zinc")
+  const [id7, setId7] = useState("zinc")
+  const [id8, setId8] = useState("zinc")
+
+  
   return (
     <>
       <div className="min-h-screen min-w-screen bg-black flex justify-center items-center">
         <div className="border-2 border-zinc-400 rounded-md">
           <div className="grid grid-cols-3 gap-4 p-4">
-            <div className="bg-zinc-100 rounded-sm  p-6 text-center">1</div>
-            <div className="bg-zinc-100 rounded-sm  p-6 text-center">2</div>
-            <div className="bg-zinc-100 rounded-sm  p-6 text-center">3</div>
-            <div className="bg-zinc-100 rounded-sm  p-6 text-center">4</div>
-            <div className="bg-zinc-100 rounded-sm  p-6 text-center">5</div>
-            <div className="bg-zinc-100 rounded-sm  p-6 text-center">6</div>
-            <div className="bg-zinc-100 rounded-sm  p-6 text-center">7</div>
-            <div className="bg-zinc-100 rounded-sm  p-6 text-center">8</div>
-            <div className="bg-red-600 rounded-sm  p-6 text-center">9</div>
+            <div
+              className={`${id1 == 'zinc' ? 'bg-zinc-100' : (id1 == 'green' ? 'bg-green-500' : 'bg-orange-400')} cursor-pointer rounded-sm  p-6 text-center transition-colors duration-300`}
+              onClick={() => { setId1("green"); setOrder(prev => [...prev, 1]); }}
+            >
+              1
+            </div>
+            <div 
+              className={`${id2 == 'zinc' ? 'bg-zinc-100' : (id2 == 'green' ? 'bg-green-500' : 'bg-orange-400')} cursor-pointer rounded-sm  p-6 text-center transition-colors duration-300`}
+              onClick={() => { setId2("green"); setOrder(prev => [...prev, 2]); }}
+            >
+              2
+            </div>
+            <div 
+              className={`${id3 == 'zinc' ? 'bg-zinc-100' : (id3 == 'green' ? 'bg-green-500' : 'bg-orange-400')} cursor-pointer rounded-sm  p-6 text-center transition-colors duration-300`}
+              onClick={() => { setId3("green"); setOrder(prev => [...prev, 3]); }}
+            >
+                3
+            </div>
+            <div 
+              className={`${id4 == 'zinc' ? 'bg-zinc-100' : (id4 == 'green' ? 'bg-green-500' : 'bg-orange-400')} cursor-pointer rounded-sm  p-6 text-center transition-colors duration-300`}
+              onClick={() => { setId4("green"); setOrder(prev => [...prev, 4]); }}
+            >
+              4
+            </div>
+            <div 
+              className={`${id5 == 'zinc' ? 'bg-zinc-100' : (id5 == 'green' ? 'bg-green-500' : 'bg-orange-400')} cursor-pointer rounded-sm  p-6 text-center transition-colors duration-300`}
+              onClick={() => { setId5("green"); setOrder(prev => [...prev, 5]); }}
+            >
+              5
+            </div>
+            <div 
+              className={`${id6 == 'zinc' ? 'bg-zinc-100' : (id6 == 'green' ? 'bg-green-500' : 'bg-orange-400')} cursor-pointer rounded-sm  p-6 text-center transition-colors duration-300`}
+              onClick={() => { setId6("green"); setOrder(prev => [...prev, 6]); }}
+            >
+              6
+            </div>
+            <div 
+              className={`${id7 == 'zinc' ? 'bg-zinc-100' : (id7 == 'green' ? 'bg-green-500' : 'bg-orange-400')} cursor-pointer rounded-sm  p-6 text-center transition-colors duration-300`}
+              onClick={() => { setId7("green"); setOrder(prev => [...prev, 7]); }}
+            >
+              7
+            </div>
+            <div 
+              className={`${id8 == 'zinc' ? 'bg-zinc-100' : (id8 == 'green' ? 'bg-green-500' : 'bg-orange-400')} cursor-pointer rounded-sm  p-6 text-center transition-colors duration-300`}
+              onClick={() => { setId8("green"); setOrder(prev => [...prev, 8]); }}
+            >
+              8
+            </div>
+            <div 
+              className={`bg-red-400 rounded-sm  p-6 text-center cursor-pointer `}
+              onClick={ () => {  }}
+            >
+              9
+            </div>
           </div>
 
         </div>
